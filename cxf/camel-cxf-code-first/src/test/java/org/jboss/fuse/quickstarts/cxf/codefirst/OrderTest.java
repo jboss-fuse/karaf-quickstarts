4f524a4ddfb6bd881868528647e79739182af36a
Grzegorz Grzybek <gr.grzybek@gmail.com>
2018-11-26 10:27:51 +0100

    [ENTESB-9721] cxf quickstarts are parentless

--- cxf/camel-cxf-code-first/src/test/java/org/jboss/fuse/quickstarts/cxf/codefirst/OrderTest.java
+++ cxf/camel-cxf-code-first/src/test/java/org/jboss/fuse/quickstarts/cxf/codefirst/OrderTest.java
@@ -13,7 +13,7 @@
  *  implied.  See the License for the specific language governing
  *  permissions and limitations under the License.
  */
-package camelinaction.order;
+package org.jboss.fuse.quickstarts.cxf.codefirst;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
